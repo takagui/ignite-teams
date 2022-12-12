5abcc6108f675df0eae36071139c94e91524721e
Guilherme Takahashi <gui.yukio175@gmail.com>
2022-12-12 17:11:26 -0300

    feat: create loading

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 
 import theme from './src/theme';
 import { Groups } from '@screens/Groups';
-import { ActivityIndicator } from 'react-native';
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -13,7 +13,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      { fontsLoaded ? <Groups /> : <ActivityIndicator /> }
+      { fontsLoaded ? <Groups /> : <Loading /> }
     </ThemeProvider>
   );
 }
