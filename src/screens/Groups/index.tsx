a957326bc8eaaa632f88cf74d2f631ec822d73cb
Guilherme Takahashi <gui.yukio175@gmail.com>
2022-12-14 11:15:22 -0300

    feat: create button component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 import { Container } from './styles';
 
@@ -30,6 +31,10 @@ export function Groups() {
         keyExtractor={item => item}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira turma?" />}
+      />
+
+      <Button
+        title="Criar nova turma"
       />
     </Container>
   );
