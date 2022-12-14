82575b96e8fe16d40efb1728b6bd3ef7ea8b70db
Guilherme Takahashi <gui.yukio175@gmail.com>
2022-12-14 12:22:14 -0300

    feat: create screen new group

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import theme from './src/theme';
 import { Groups } from '@screens/Groups';
+import { NewGroup } from '@screens/NewGroup';
+
 import { Loading } from '@components/Loading';
-import { StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -19,7 +21,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Groups /> : <Loading /> }
+      { fontsLoaded ? <NewGroup /> : <Loading /> }
     </ThemeProvider>
   );
 }
