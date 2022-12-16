77593f4f458b0991509a4ef481b782e19f2ca644
Guilherme Takahashi <gui.yukio175@gmail.com>
2022-12-15 23:24:12 -0300

    feat: implement navigation

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import theme from './src/theme';
-import { Groups } from '@screens/Groups';
+import { Routes } from './src/routes';
 
 import { Loading } from '@components/Loading';
 
@@ -20,7 +20,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Groups /> : <Loading /> }
+      { fontsLoaded ? <Routes /> : <Loading /> }
     </ThemeProvider>
   );
 }
